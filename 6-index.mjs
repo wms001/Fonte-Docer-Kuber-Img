c727ac03c8cebb023494c0d912af1353161b6b69
Wilmar Santos <wilmar.santos@gmail.com>
2022-07-28 16:55:46 -0300

    Add files via upload

---
+++ 6-index.mjs
@@ -0,0 +1,9 @@
+// Crie o arquivo package.json com o comando npm init -y
+import {createServer} from 'node:http'
+
+const server = createServer((r, rs) => {
+  rs.writeHead(200, { 'Content-Type': 'application/json' })
+  rs.end('Hello')
+})
+
+server.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`))
